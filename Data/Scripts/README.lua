ba0ae7b99fed0cdf2c13d15aded0a7c0f7d33558
CommanderFoo <pixeldepth@gmail.com>
2022-10-19 13:25:41 +0100

    Update README.lua

--- Data/Scripts/README.lua
+++ Data/Scripts/README.lua
@@ -1,12 +1,12 @@
 --[[
-  _______   _                       _            
- |__   __| | |                     | |           
-    | | ___| | ___ _ __   ___  _ __| |_ ___ _ __ 
+  _______   _                       _
+ |__   __| | |                     | |
+    | | ___| | ___ _ __   ___  _ __| |_ ___ _ __
     | |/ _ \ |/ _ \ '_ \ / _ \| '__| __/ _ \ '__|
-    | |  __/ |  __/ |_) | (_) | |  | ||  __/ |   
-    |_|\___|_|\___| .__/ \___/|_|   \__\___|_|   
-                  | |                            
-                  |_|  
+    | |  __/ |  __/ |_) | (_) | |  | ||  __/ |
+    |_|\___|_|\___| .__/ \___/|_|   \__\___|_|
+                  | |
+                  |_|
 ----------------------------------------------------
 
 The Teleporter component allows creators to set up teleporters around their map to help
@@ -15,13 +15,19 @@ players quickly travel between different areas.
 The component uses a data table to make connecting teleporters quick and simple.
 -----------------------------------------------------------------------------------------------------------------
 
+====
+NOTE
+====
+
+If the Dependent folders are empty in Project Content under Imported Content for this component, save and reload the project to fix it.
+
 ==========
 How to use
 ==========
 
 An example template is included that contains 2 teleporters that are linked to each other. When
 the player enters the outside teleporter, the player will be teleported to the inside teleporter
-in the cafe. 
+in the cafe.
 
 To create your own teleporters, there is a Teleporter template that can be used. The template contains
 2 custom properties on the root of the template.
